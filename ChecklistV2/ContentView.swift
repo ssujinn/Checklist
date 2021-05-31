da3747cb6e469e18fcb6ef63d5d524cddc4036de
ssujinn <efsonata38@gmail.com>
2021-05-31 22:41:38 +0900

    change the item of list by .onTapGesture

--- ChecklistV2/ContentView.swift
+++ ChecklistV2/ContentView.swift
@@ -8,36 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var checklistItems = ["Walk the dog", "Brush my teeth", "Learn iOS development", "Soccer practice", "Eat ice cream"]
+    
     var body: some View {
         NavigationView {
             List {
-                Section(header: Text("Hight priority")){
-                    Group {
-                        Text("Walk the dog")
-                        Text("Brush my teeth")
-                        Text("Learn iOS development")
-                        Text("Make dinner")
-                        Text("Do laundry")
-                        Text("Pay bills")
+                Text(checklistItems[0])
+                    .onTapGesture {
+                        self.checklistItems[0] = "Take the dog to the vet"
                     }
-                    Group {
-                        Text("Finish homework")
-                        Text("Change internet provider")
-                        Text("Clean the kitchen")
-                        Text("Wash the car")
-                        Text(" Wash the hair")
-                    }
-                }
-                Section(header: Text("Low priority")){
-                    Text("Soccer practice")
-                    Text("Eat Ice cream")
-                }
-            } // End of body
+                Text(checklistItems[1])
+                Text(checklistItems[2])
+                Text(checklistItems[3])
+                Text(checklistItems[4])
+            } // End of list
             .listStyle(GroupedListStyle())
             .navigationBarTitle("Checklist")
         } // End of navigationView
-    }
-}
+    } // End of body
+} // End of ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

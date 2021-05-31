4533a50f6ef6950b93218d48072e82e598c5124b
ssujinn <efsonata38@gmail.com>
2021-05-31 21:51:51 +0900

    Initial Commit

---
+++ ChecklistV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChecklistV2
+//
+//  Created by RelMac User Exercise2 on 2021/05/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

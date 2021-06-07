48598229781c2041416fd89151d321b74e05753e
ssujinn <efsonata38@gmail.com>
2021-06-08 02:45:40 +0900

    connect by NavigationLink()

---
+++ ChecklistV2/RowView.swift
@@ -0,0 +1,28 @@
+//
+//  RowView.swift
+//  ChecklistV2
+//
+//  Created by RelMac User Exercise2 on 2021/06/08.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    @State var checklistItem: ChecklistItem
+    
+    var body: some View {
+        NavigationLink(destination: EditChecklistItemView()){
+            HStack {
+                Text(checklistItem.name)
+                Spacer()
+                Text(checklistItem.isChecked ? "☑️" : "⬛️")
+            }   // End of HStack
+        }   // End of NaviationLink
+    }   // End of View
+}
+
+struct RowView_Previews: PreviewProvider {
+    static var previews: some View {
+        RowView(checklistItem: ChecklistItem(name: "sample item"))
+    }
+}
